f427a2aefd6799b1ac936ce3bd5a820efc2fd93a
Richard Pike <drawingboyrp@gmail.com>
2024-07-06 09:22:16 -0400

    cleaned up vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import { fileURLToPath } from 'url'
-// import { resolve } from 'path'
-
-// const __filename = fileURLToPath(import.meta.url);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // assetsInclude: ["**/tilemapresource.xml"],
   build: {
     rollupOptions: {
       external: [
-        // new RegExp(resolve('datadump-merging', __filename)),
         'datadump-merging/**',
       ],
     },
